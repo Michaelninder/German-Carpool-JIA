a946dd5653fca09f6c2f5931df0d2e785052d57d
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-11-18 15:08:36 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Best Rute x HHG-kl.de | Fahrgemeinschaft</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.css" />
+    <link rel="stylesheet" href="assets/styles/map.php" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.css" />
+</head>
+<body>
+
 
 <?php
 import "includes/map.php";
